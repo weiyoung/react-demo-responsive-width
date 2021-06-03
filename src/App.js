d693046e0aeada87091cc8943614b5ca6764d82c
Weiyoung Tan <wtan@bgcengineering.ca>
2021-06-02 23:11:12 -0700

    added useEffect and EventListener

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import React, { useState, useEffect } from 'react'
+
 export default function App() {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  const handleResize = () => {
+    setWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize) // everytime 'resize' happens, handleResize get's called
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    } // we need to remove the EventListener, otherwise there will be more and more EventListeners added
+
+  }, []) // empty array means useEffect() runs once, at the start
+
   return (
     <div className="App">
+      The screen width is {width}
+      <br/>
       Try resizing the screen width!
     </div>
   );
